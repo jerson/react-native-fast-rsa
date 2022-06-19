97d0be9704e7fb84af338a38703d8a4434e137a7
Gerson Alexander Pardo Gamez <jeral17@gmail.com>
2022-06-19 09:21:21 -0500

    Applied patch from /#46

--- android/src/main/java/com/reactnativefastrsa/FastRsaModule.kt
+++ android/src/main/java/com/reactnativefastrsa/FastRsaModule.kt
@@ -62,7 +62,9 @@ internal class FastRsaModule(reactContext: ReactApplicationContext) :
 
     override fun initialize() {
         super.initialize()
-        initialize(this.reactApplicationContext.javaScriptContextHolder.get())
+        reactApplicationContext.runOnJSQueueThread {
+           initialize(this.reactApplicationContext.javaScriptContextHolder.get())
+        }
     }
 
     override fun onCatalystInstanceDestroy() {
