5084d09e570762303d0b125d9a60a0c4023d1218
Gerson Alexander Pardo Gamez <jerson@users.noreply.github.com>
2025-03-15 07:40:38 -0500

    feat: add bytes methods and using embeded TextEncoder and TextDecoder (#95)
    * feat: add bytes methods and using embeded TextEncoder and TextDecoder
    
    * chore: remove extra .js
    
    * chore: fix types

---
+++ src/utils/text-decoder.ts
@@ -0,0 +1,53 @@
+import { NativeModules } from 'react-native';
+
+const FastRSANativeModules = (NativeModules as NativeModulesDef).FastRsa;
+const isDebuggingEnabled = typeof atob !== 'undefined';
+
+typeof global.FastRSADecodeText === 'undefined' &&
+  !isDebuggingEnabled &&
+  FastRSANativeModules.install();
+
+interface TextDecoderOptions {
+  fatal?: boolean;
+  ignoreBOM?: boolean;
+}
+
+interface TextDecodeOptions {
+  stream?: boolean;
+}
+
+export default class TextDecoder {
+  private encoding: string;
+  private fatal: boolean;
+  private ignoreBOM: boolean;
+
+  constructor(label: string = 'utf-8', options: TextDecoderOptions = {}) {
+    this.encoding = label.toLowerCase();
+    this.fatal = options.fatal ?? false;
+    this.ignoreBOM = options.ignoreBOM ?? false;
+  }
+
+  decode(input?: Uint8Array, options: TextDecodeOptions = {}): string {
+    if (!input) {
+      return '';
+    }
+
+    if (typeof global.FastRSADecodeText === 'function') {
+      const ress = global.FastRSADecodeText(
+        input,
+        this.encoding,
+        this.fatal,
+        this.ignoreBOM,
+        options.stream ?? false
+      );
+      return ress;
+    }
+    return FastRSANativeModules.decodeText(
+      Array.from(input),
+      this.encoding,
+      this.fatal,
+      this.ignoreBOM,
+      options.stream ?? false
+    );
+  }
+}
